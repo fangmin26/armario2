d45a2fb4dd38af353e4fbd52418c8868752240e5
tmdeo <tmdeo93@naver.com>
2021-07-24 19:19:14 +0900

    1111

---
+++ src/components/Ham.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import $ from 'jquery';
+
+class App extends React.Component {
+  componentDidMount(){
+    $('.hamburger-button').on('click', function(event) {
+      event.preventDefault();
+      $(this).toggleClass('active');
+      $('.overlay').toggleClass('visible');
+  });
+  }
+  render(){
+    return (
+      <>
+      <Link to="#" class="hamburger-button">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </Link>
+
+      <div class="overlay">
+        <nav class="menu">
+          <ul>
+            <li>
+              <Link to="/">ARMARIO</Link>
+            </li>
+            <li>
+              <Link to="./review">REVIEW</Link>
+            </li>
+            <li>
+              <Link to="./service">SERVICE</Link>
+            </li>
+            <li>
+              <Link to="./event">EVENT</Link>
+            </li>
+            <li>
+              <Link to="./store">STORE</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+    );
+  }
+}
+
+export default App;
+
